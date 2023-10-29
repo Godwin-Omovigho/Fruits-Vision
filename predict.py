a11fa50c25e3fa426a6b3c3d7be87dec4d75e581
Godwin-Omovigho <108032623+Godwin-Omovigho@users.noreply.github.com>
2023-10-29 17:06:20 +0100

    Fruits vision files
    This repository contains the code/steps to build the Fruits Vision Model and the code/steps to build the app.

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 import streamlit as st
-# import tensorflow as tf
-# import matplotlib.pyplot as plt
-# import matplotlib.image as mpimg
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import pathlib
 import numpy as np
 from PIL import Image
